5d0c723e8034fe291d11b49fb9a7089e4a5c809f
NL <nl@NLs-MacBook-Pro.local>
2019-12-18 14:06:43 +1100

    Information model and controller

---
+++ kubernetesapp/Controllers/InfoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KubernetesApp.Models;
+
+namespace KubernetesApp.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class InfoController : ControllerBase
+    {
+        private readonly ILogger<InfoController> _logger;
+
+        public InfoController(ILogger<InfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Information GetInfo()
+        {
+            return new Information { AppEnvironment = GetEnvironmentVariable("APPENVIRONMENT"), AppHost = GetEnvironmentVariable("APPHOST") };
+        }
+
+        private string GetEnvironmentVariable(string name)
+        {
+            _logger.LogInformation($"Getting environment variable '{name}'.");
+            return Environment.GetEnvironmentVariable(name.ToLower()) ?? Environment.GetEnvironmentVariable(name.ToUpper());
+        }
+    }
+}
---
+++ kubernetesapp/Models/Information.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace KubernetesApp.Models
+{
+    public class Information
+    {
+        public string AppEnvironment { get; set; }
+        public string AppHost { get; set; }
+    }
+}
